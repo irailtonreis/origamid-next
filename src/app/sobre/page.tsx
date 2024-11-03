1d258d1e202ae7d7143821058f7261883ef394d3
irailtonreis <irailton.oliveira91@gmail.com>
2024-11-03 11:57:25 -0300

    feat: Estrutura inicial

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,18 @@
+import Width from "@/components/width";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Página Sobre',
+    description: 'Essa é a descrição da página sobre',
+  };
+  
+export default function SobrePage() {
+    return (
+      <main>
+        <h1>Sobre</h1>
+        A Empresa
+      <Width/>
+      </main>
+    );
+  }
+  
